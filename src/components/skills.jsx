8f186e0f8d9dd2e20e2051b8b543543363c20d95
Andy Lewis <andy.c.lewis@gmail.com>
2025-02-15 21:51:00 -0700

    Add glowing pulse circle bg animation component, added to homepage components

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,6 +1,7 @@
 import DevSkills from './skills-dev';
 import DesignSkills from './skills-design';
 import PlatformSkills from './skills-platform-tools';
+import GlowPulse from './glow-pulse';
 // import TwinkleStars from './twinkle-stars';
 // import { FaBolt } from 'react-icons/fa6';
 
@@ -8,6 +9,7 @@ const Skills = () => {
   return (
     <section className="skills">
       <div className="container">
+        <GlowPulse position="left small" />
         <h2>Languages & Tools</h2>
         <div className="skills-content container">
           {/* <div className="skills-content__group">
