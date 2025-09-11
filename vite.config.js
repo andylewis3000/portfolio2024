4c4504c82d8a15ab858f423f766a5737695e7852
Andy Lewis <andy.c.lewis@gmail.com>
2025-09-11 12:23:34 -0600

    moves images references in css to public file in hero and mobile nav, removes vite svg

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // Uncomment this line when bulding for production -- needs to match browserRouter 'basename' path in 'main.jsx'
-  // base: '/',
+  base: '/',
   plugins: [react()],
   build: {
     sourcemap: true, // Enable production source maps
